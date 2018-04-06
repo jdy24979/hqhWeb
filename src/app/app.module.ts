72fd84b3dd03e018619a9c9ca47852707a55bad6
Jdy <249792185@qq.com>
2018-04-06 09:44:07 +0800

    和庆和2018.4.6

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NgZorroAntdModule.forRoot()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
