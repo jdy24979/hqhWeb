f5af97723a77f73f51bda1c9a222889561c3bc43
S7IHQ6T8OCIVLZ5\Administrator <249792185@qq.com>
2018-06-19 22:21:41 +0800

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,48 +1,49 @@
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
+import { BillAddComponent } from "./components/pages/bill/bill-add/bill-add.component";
+import { BillDetailComponent } from "./components/pages/bill/bill-detail/bill-detail.component";
+import { BillListComponent } from "./components/pages/bill/bill-list/bill-list.component";
+import { BillTotalComponent } from "./components/pages/bill/bill-total/bill-total.component";
+import { PriceHistoryComponent } from "./components/pages/price/price-history/price-history.component";
+import { PriceListComponent } from "./components/pages/price/price-list/price-list.component";
+import { PriceOptionComponent } from "./components/pages/price/price-option/price-option.component";
+import { ModelComponent } from "./components/pages/product/model/model.component";
+import { ProductComponent } from "./components/pages/product/product/product.component";
+import { SpecComponent } from "./components/pages/product/spec/spec.component";
 import { GoodListComponent } from "./components/pages/storehouse/good-list/good-list.component";
 import { GoodsInComponent } from "./components/pages/storehouse/goods-in/goods-in.component";
 import { GoodsOutComponent } from "./components/pages/storehouse/goods-out/goods-out.component";
 import { SheetComponent } from "./components/pages/storehouse/sheet/sheet.component";
 
-import { PriceListComponent } from "./components/pages/price/price-list/price-list.component";
-import { PriceOptionComponent } from "./components/pages/price/price-option/price-option.component";
-import { PriceHistoryComponent } from "./components/pages/price/price-history/price-history.component";
 
-import { BillTotalComponent } from"./components/pages/bill/bill-total/bill-total.component";
-import { BillListComponent } from"./components/pages/bill/bill-list/bill-list.component";    
-import { BillDetailComponent } from"./components/pages/bill/bill-detail/bill-detail.component";
-import { BillAddComponent } from"./components/pages/bill/bill-add/bill-add.component";
 
-import { ProductComponent } from "./components/pages/product/product/product.component";
-import { ModelComponent } from "./components/pages/product/model/model.component";
-import { SpecComponent } from "./components/pages/product/spec/spec.component";
 
-const routes: Routes = [
-    {path:"",component:GoodListComponent},
-    {path:"home/storehouse",component:GoodListComponent},
-    {path:"home/storehouse/goodsList",component:GoodListComponent},
-    {path:"home/storehouse/goodsIn",component:GoodsInComponent},
-    {path:"home/storehouse/goodsOut",component:GoodsOutComponent},
-    {path:"home/storehouse/sheet",component:SheetComponent},
-    {path:"home/price",component:PriceListComponent},
-    {path:"home/price/list",component:PriceListComponent},
-    {path:"home/price/option",component:PriceOptionComponent},
-    {path:"home/price/history",component:PriceHistoryComponent},
-    {path:"home/system",component:PriceListComponent},
-    {path:"home/system/users",component:PriceListComponent},
-    {path:"home/system/role",component:PriceOptionComponent},
-    {path:"home/system/dictionary",component:PriceHistoryComponent},
-    {path:"home/bill",component:BillTotalComponent},
-    {path:"home/bill/billTotal",component:BillTotalComponent},
-    {path:"home/bill/list",component:BillListComponent},
-    {path:"home/bill/detail",component:BillDetailComponent},
-    {path:"home/bill/add",component:BillAddComponent},
-    {path:"home/product",component:ProductComponent},
-    {path:"home/product/product",component:ProductComponent},
-    {path:"home/product/model",component:ModelComponent},
-    {path:"home/product/spec",component:SpecComponent}
+const routelist = [
+    { path: "", component: GoodListComponent },
+    { path: "home/storehouse", component: GoodListComponent },
+    { path: "home/storehouse/goodsList", component: GoodListComponent },
+    { path: "home/storehouse/goodsIn", component: GoodsInComponent },
+    { path: "home/storehouse/goodsOut", component: GoodsOutComponent },
+    { path: "home/storehouse/sheet", component: SheetComponent },
+    { path: "home/price", component: PriceListComponent },
+    { path: "home/price/list", component: PriceListComponent },
+    { path: "home/price/option", component: PriceOptionComponent },
+    { path: "home/price/history", component: PriceHistoryComponent },
+    { path: "home/system", component: PriceListComponent },
+    { path: "home/system/users", component: PriceListComponent },
+    { path: "home/system/role", component: PriceOptionComponent },
+    { path: "home/system/dictionary", component: PriceHistoryComponent },
+    { path: "home/bill", component: BillTotalComponent },
+    { path: "home/bill/billTotal", component: BillTotalComponent },
+    { path: "home/bill/list", component: BillListComponent },
+    { path: "home/bill/detail", component: BillDetailComponent },
+    { path: "home/bill/add", component: BillAddComponent },
+    { path: "home/product", component: ProductComponent },
+    { path: "home/product/product", component: ProductComponent },
+    { path: "home/product/model", component: ModelComponent },
+    { path: "home/product/spec", component: SpecComponent }
 ];
+const routes: Routes = routelist;
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
